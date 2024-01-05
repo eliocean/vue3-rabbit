3918dc9ed6246e4e4638ea5820ef5799f92f7ee5
eliocean <ocean_yyl@163.com>
2024-01-05 15:34:33 +0800

    别名联想设置

---
+++ vite.config.js
@@ -0,0 +1,17 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+  ],
+  resolve: {
+    alias: {
+      // 程序实际路径转换: @ -> ./src/
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
+})
