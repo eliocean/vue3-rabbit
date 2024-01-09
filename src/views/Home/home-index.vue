72a275ad55b5933ec8787e6dfa297611dadcf307
eliocean <ocean_yyl@163.com>
2024-01-09 23:11:01 +0800

    整体结构拆分和分类实现2

--- src/views/Home/home-index.vue
+++ src/views/Home/home-index.vue
@@ -1,6 +1,18 @@
-<template>
-    <div>Home</div>
-    <div style="height: 1000px;">
+<script setup>
+import HomeBanner from './components/HomeBanner.vue'
+import HomeCategory from './components/HomeCategory.vue'
+import HomeNew from './components/HomeNew.vue'
+import HomeHot from './components/HomeHot.vue'
+import HomeProduct from './components/HomeProduct.vue'
 
+</script>
+
+<template>
+    <div class="container">
+        <HomeCategory />
+        <HomeBanner />
     </div>
+    <HomeNew />
+    <HomeHot />
+    <HomeProduct />
 </template>
