a59ee2d607785a46f955e4cb065df5cd43e19b4d
eliocean <ocean_yyl@163.com>
2024-01-10 20:01:09 +0800

    Home-新鲜好物和人气推荐实现

--- src/apis/home.js
+++ src/apis/home.js
@@ -7,3 +7,24 @@ export function getBannerAPI() {
     });
 }
 
+/**
+ * 获取新鲜好物
+ * @returns 
+ */
+export function findNewAPI() {
+    return httpInstance({
+        url: '/home/new',
+        method: 'GET'
+    });
+}
+
+/**
+ * 获取人气推荐
+ * @returns 
+ */
+export const getHotAPI = () => {
+    return httpInstance({
+        url: '/home/hot',
+        method: 'GET'
+    })
+}
