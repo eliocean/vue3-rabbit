c44b84a541f1f19572ee1006784a8c1a923df6ef
eliocean <ocean_yyl@163.com>
2024-01-11 16:24:20 +0800

    GoodItem 组件封装

--- src/views/Home/components/HomeProduct.vue
+++ src/views/Home/components/HomeProduct.vue
@@ -2,6 +2,7 @@
 import { onMounted, ref } from 'vue';
 import HomePanel from './HomePanel.vue'
 import { getGoodsAPI } from '@/apis/home';
+import GoodItem from './GoodItem.vue';
 
 const goodsProduct = ref([]);
 const getGoods = async () => {
@@ -21,18 +22,13 @@ onMounted(() => getGoods());
                 <RouterLink class="cover" to="/">
                     <img v-img-lazy="cate.picture" />
                     <strong class="label">
-                        <span>{{ cate.name }}馆</span>
+                        <span>{{ cate.name }}</span>
                         <span>{{ cate.saleInfo }}</span>
                     </strong>
                 </RouterLink>
                 <ul class="goods-list">
                     <li v-for="good in cate.goods" :key="good.id">
-                        <RouterLink to="/" class="goods-item">
-                            <img v-img-lazy="good.picture" alt="" />
-                            <p class="name ellipsis">{{ good.name }}</p>
-                            <p class="desc ellipsis">{{ good.desc }}</p>
-                            <p class="price">&yen;{{ good.price }}</p>
-                        </RouterLink>
+                        <GoodItem :good="good" />c
                     </li>
                 </ul>
             </div>
