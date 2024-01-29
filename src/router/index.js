665906ad9a3ae15d16b0c9b851af9e60c80c7cd1
eliocean <ocean_yyl@163.com>
2024-01-29 17:15:42 +0800

    登录——整体认识和路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Login from '@/views/login/login-index.vue'
+import Login from '@/views/login/index.vue'
 import Layout from '@/views/layout/layout-index.vue'
 import Home from '@/views/Home/home-index.vue'
 import Category from '@/views/Category/category-index.vue'
