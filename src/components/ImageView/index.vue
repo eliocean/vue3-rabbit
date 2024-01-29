6800fe26ce6245d99cf9e3e547d5c2fb0878a943
eliocean <ocean_yyl@163.com>
2024-01-29 16:05:48 +0800

    放大镜——滑块跟随鼠标移动

--- src/components/ImageView/index.vue
+++ src/components/ImageView/index.vue
@@ -1,6 +1,7 @@
 
 <script setup>
-import { ref } from 'vue';
+import { useMouseInElement } from '@vueuse/core';
+import { ref, watch } from 'vue';
 // 图片列表
 const imageList = [
     "https://yanxuan-item.nosdn.127.net/d917c92e663c5ed0bb577c7ded73e4ec.png",
@@ -18,16 +19,47 @@ const enterHandler = (index) => {
     // console.log(activeImageIndex.value);
 }
 
+
+//获取鼠标在盒子的相对位置
+const target = ref(null);
+const left = ref(0);
+const top = ref(0);
+const { elementX, elementY, isOutside } = useMouseInElement(target);
+watch([elementX, elementY], () => {
+    console.log("鼠标位置变化了");
+    if (elementX.value > 100 && elementX.value < 300) {
+        left.value = elementX.value - 100; // 设置滑块的left值，使其跟随鼠标移动
+    }
+    if (elementY.value > 100 && elementY.value < 300) {
+        top.value = elementY.value - 100; // 设置滑块的top值，使其跟随鼠标移动
+    }
+    if (elementX.value < 100) {
+        left.value = 0;
+    }
+    if (elementX.value > 300) {
+        left.value = 200;
+    }
+    if (elementY.value < 100) {
+        top.value = 0;
+    }
+    if (elementY.value > 300) {
+        top.value = 200;
+    }
+})
+
 </script>
 
 
 <template>
+    {{ elementX }} <br>
+    {{ elementY }} <br>
+    {{ isOutside }}
     <div class="goods-image">
         <!-- 左侧大图-->
         <div class="middle" ref="target">
             <img :src="imageList[activeImageIndex]" alt="" />
             <!-- 蒙层小滑块 -->
-            <div class="layer" :style="{ left: `0px`, top: `0px` }"></div>
+            <div class="layer" :style="{ left: `${left}px`, top: `${top}px` }"></div>
         </div>
         <!-- 小图列表 -->
         <ul class="small">
